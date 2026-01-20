375ad033f28ca8231faf273cd85b8c2f0af3e961
dmittrey <dmitryzubahin@gmail.com>
2026-01-20 16:30:46 +0300

    fixup! state: Remove stack size parameter in ctor

--- interpreter/main.c
+++ interpreter/main.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[]) {
     }
 
     /* Create interpreter state with 64KB stack */
-    interpreter_state_t *state = create_interpreter_state(bf, 64 * 1024);
+    interpreter_state_t *state = create_interpreter_state(bf);
     if (!state) {
         fprintf(stderr, "Failed to create interpreter state\n");
         return 1;
