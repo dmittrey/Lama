03b2bca4feda3964058bc18ac0c51d648c3d2160
dmittrey <dmitryzubahin@gmail.com>
2026-01-25 05:50:15 +0300

    vm: implement RET
    Signed-off-by: dmittrey <dmitryzubahin@gmail.com>

--- vm/vm.c
+++ vm/vm.c
@@ -185,7 +185,17 @@ static error_code_e op_end(FILE *f, struct interpreter_state_t *state, char l) {
 }
 
 static error_code_e op_ret(FILE *f, struct interpreter_state_t *state, char l) {
-  DBG("RET");
+  csval_t callee_ret;
+  uint32_t ret_off;
+  RETURN_IF_ERROR(callstack_pop_operand(state_cs(state), &callee_ret));
+  RETURN_IF_ERROR(callstack_pop_frame(state_cs(state), &ret_off));
+  if (callstack_nframes(state_cs(state)) == 0) {
+    return ERROR_STOP;
+  }
+  DBG("RET\t%p", ret_off);
+  RETURN_IF_ERROR(callstack_push_operand(
+      state_cs(state), callee_ret)); // Put retval on caller stack
+  RETURN_IF_ERROR(state_jmp(state, ret_off));
   return ERROR_NONE;
 }
 
